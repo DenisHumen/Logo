292e6c17774247c4b15321c6d115d76445118122
Krokosha <56129242+MeSmallMan@users.noreply.github.com>
2024-09-08 20:34:20 +0300

    Create logo0.sh
    new logo

---
+++ logo0.sh
@@ -0,0 +1,15 @@
+function colors {
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+colors
+
+echo -e "${YELLOW} ____ ____ ____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ ____ ${NORMAL}"
+echo -e "${YELLOW}||K |||r |||o |||k |||o |||s |||h |||a |||       |||C |||o |||m |||a |||n |||d ||${NORMAL}"
+echo -e "${YELLOW}||__|||__|||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__||${NORMAL}"
+echo -e "${YELLOW}|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|${NORMAL}"
+echo
+echo -e "${YELLOW}KrokoshaComand${NORMAL}"
